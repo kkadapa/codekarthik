071f90631a279cd13f820928c0a16ffbc6d315f6
kkadapa <karthikkadapa88@gmail.com>
2015-11-18 08:53:00 -0600

    compressString with counts and uniquecharsinarray programs

---
+++ src/KStackArray.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Spectre on 11/16/15.
+ */
+public class KStackArray {
+
+    private String[] stackArray;
+    private int stackSize;
+
+    //Setting the size of the stack empty
+    private int topOfStack = -1;
+
+    KStackArray(int size){
+
+        stackSize = size;
+        stackArray = new String[size];
+
+    }
+
+    public static void push (String input){
+
+
+
+
+    }
+
+    public static void pop () {
+
+
+
+    }
+}
